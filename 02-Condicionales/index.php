b2782100bdf0bac0c7001b73ad66de23ecac2717
trabajo final <valentinamazoblandon@gmail.com>
2024-05-10 17:31:19 -0500

    feat:Sintexis-variables

---
+++ 02-Condicionales/index.php
@@ -0,0 +1,55 @@
+<?php
+
+$articulos = '12';
+
+if($articulos>12) {
+
+    echo 'Caja rápida';
+}
+
+else if($articulos<12) {
+
+    echo 'Caja normal';
+}
+
+$ciudadanos = '18';
+
+if($ciudadanos>18) {
+
+    echo 'Puedes votar';
+
+}
+
+else if($ciudadanos<18) {
+
+    echo 'No puedes votar';
+}
+
+$nombre = 'Mario, Carlos';
+$edad = '19';
+
+if($nombre == Mario,Carlos) {
+
+    echo 'Puedes entrar al VIP';
+
+}
+
+else if($nombre<>Mario,Carlos) {
+
+    echo 'No puedes entrar';
+
+}
+
+if($edad<19) {
+
+    echo 'Si puedes entrar';
+}
+
+else if($edad>19) {
+
+    echo 'No puedes entrar';
+}
+
+
+
+?>
