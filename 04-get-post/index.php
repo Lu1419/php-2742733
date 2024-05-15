be1a8163275a853fcbe7d96c3421370ec6963a94
Lucia <lucycortes1009@gmail.com>
2024-05-15 15:52:53 -0500

    feat geat-post

---
+++ 04-get-post/index.php
@@ -0,0 +1,41 @@
+<?php
+
+    //superglobal GEY y POST
+    $userName = $_GET['username'];
+    $userEmail = $_GET['usermail'];
+
+    echo $userName;
+    echo '<br>';
+    echo $userEmail;
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        body{
+            background-color: <?php echo $_POST['color'] ?>}
+
+    </style>
+</head>
+<body>
+    
+    <form action="index.php" method="post">
+    <label for="username">Username</label>
+    <input id="username" type="text" placeholder="Username..." name="username">
+    <br>
+    <label for="usermail" required>Usermail</label>
+    <input id="usermail"  type="email"placeholder="Usermail..." required name="useremail">
+    <br>
+
+    <label for="color">Color</label>
+    <input id="color" type="text" name="color" placeholder="Color para el fondo...">
+    <button type="submit">Enviar</button>
+    </form>
+</body>
+</html>
