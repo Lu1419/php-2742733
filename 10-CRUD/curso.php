3a8a6398c005a94491e297de4769ef0e878be4d9
Lucia <lucycortes1009@gmail.com>
2024-06-27 17:09:29 -0500

    feat: CRUD

---
+++ 10-CRUD/curso.php
@@ -0,0 +1,79 @@
+<?php
+require('conexion.php');
+require('header.php');
+
+?>
+
+<div class="container">
+    <h2>Formulario de Registro</h2>
+
+    <?php if (isset($_SESSION['mensaje'])): ?>
+        <div class="row">
+            <div class="col">
+                <div class="alert alert-<?php echo htmlspecialchars($_SESSION['color']); ?> alert-dismissible fade show" role="alert">
+                    <p class="mb-0"><?php echo htmlspecialchars($_SESSION['mensaje']); ?></p>
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>
+            </div>
+        </div>
+        <?php session_unset(); ?>
+    <?php endif; ?>
+
+    <form action="agregar_curso.php" method="post" enctype="multipart/form-data">
+        <div class="mb-3">
+            <label for="Imagen" class="form-label">Imagen</label>
+            <input type="file" id="Imagen" name="Imagen" class="form-control" required>
+        </div>
+        <div class="mb-3">
+            <label for="Titulo" class="form-label">Título</label>
+            <input type="text" id="Titulo" name="Titulo" class="form-control" required>
+        </div>
+        <div class="mb-3">
+            <label for="Descripcion" class="form-label">Descripción</label>
+            <input type="text" id="Descripcion" name="Descripcion" class="form-control" required>
+        </div>
+        <div class="mb-3">
+            <label for="Estudiantes" class="form-label">Estudiantes</label>
+            <input type="text" id="Estudiantes" name="Estudiantes" class="form-control" required>
+        </div>
+        <button type="submit" class="btn btn-primary">Registrarse</button>
+    </form>
+</div>
+
+<h2>Cursos de Lectura</h2>
+<table class="table">
+    <thead>
+        <tr>
+            <th>ID</th>
+            <th>Imagen</th>
+            <th>Título</th>
+            <th>Descripción</th>
+            <th>Estudiantes</th>
+            <th>Acción</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php
+        $statement = $conexion->prepare("SELECT * FROM cursos");
+        $statement->execute();
+        $result = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+        foreach ($result as $item): ?>
+            <tr>
+                <td><?php echo htmlspecialchars($item['id']); ?></td>
+                <td><img src="<?php echo htmlspecialchars($item['imagen']); ?>" width="100" alt=""></td>
+                <td><?php echo htmlspecialchars($item['titulo']); ?></td>
+                <td><?php echo htmlspecialchars($item['descripcion']); ?></td>
+                <td><?php echo htmlspecialchars($item['estudiantes']); ?></td>
+                <td class="d-flex gap-2">
+                    <a class="text-success" href="editar_curso.php?id=<?php echo $item['id']; ?>"><i class="bi bi-pencil-fill"></i></a>
+                    <a class="text-danger" href="borrar_curso.php?id=<?php echo $item['id']; ?>"><i class="bi bi-trash2-fill"></i></a>
+                </td>
+            </tr>
+        <?php endforeach; ?>
+    </tbody>
+</table>
+
+
+
+<?php require('footer.php'); ?>
--- 10-CRUD/index.php
+++ 10-CRUD/index.php
@@ -1,3 +1,4 @@
+<?php require('conexion.php'); ?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -5,35 +6,46 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="./style.css">
+    <link rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 
 <body>
-    <div class="contenedor_grande">
-        <div class="contenedor">
-            <div>
-                <h1 class="titulo">Página de inicio</h1>
-            </div>
 
-            <div>
-                <form action="index.php" method="post">
-                <div class="user"><label for="user">User</label></div>
-                <input class="linea" id="user" type="text" name="user">
-            </div>
-            <br>
-            <br>
-            <div class="pass"><label for="password">Password</label></div>
-            <input class="linea" id="password" type="password" name="password">
-            <br>
-            <br>
-            <div class="boton1">
-            <button class="boton" type="submit">Iniciar sesión</button></div>
-            </form>
-            <br>
-            <div class="registro"><a class="registro" href="./registro.php">Registrate</a></div>
-    </div>
 
-    
+        <div class="container text-center">
+            <div class="row align-items-center">
+                <?php
+                $statement = $conexion->prepare("SELECT * FROM cursos");
+                $statement->execute();
+                $result = $statement->fetchAll();
+
+
+                foreach ($result as $item) { ?>
+
+                    <div class="col">
+                        <a class="no-link" href="producto.php?id=<?php echo $item['id'] ?>">
+                            <div class="card" style="width: 18rem;">
+                                <img src="<?php echo $item['imagen'] ?>" class="card-img-top" alt="...">
+                                <div class="card-body">
+                                    <h5 class="card-title"> <?php echo $item['titulo'] ?> </h5>
+                                    <p class="card-text"> <?php echo $item['descripcion'] ?> </p>
+                                    <div><i class="bi bi-people-fill"></i><span><?php echo $item['estudiantes'] ?></span></div>
+                                </div>
+                        </a>
+
+            
+                    </div>
+        </div>
+
+        <?php }  ?>
+
+        <h1 class="bienvenido">BIENVENIDO</h1>
+        <button class="boton center" link="curso.php" type="button" ><a href="./curso.php">Ingresar</a></button>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 
 </html>
